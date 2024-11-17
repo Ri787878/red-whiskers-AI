84d9b38ef2a9652eaf751b6d839389b0667273fb
Ri787878 <ricardosdias@live.com>
2024-11-17 15:02:47 +0000

    everything ready for the presentation within expected parameters ahhhhhhhh

--- mainWDisplay.py
+++ mainWDisplay.py
@@ -8,10 +8,12 @@ HEIGHT = 800
 CELL_SIZE = 10
 
 while True:
-	#Start Position
-	startPos = [ROWS - 1, 54]
 	choice = display.chooseBot()
+	#posX = input("Enter the X position of the starting point: ")
 
+	#Start Position
+	#startPos = [ROWS - 1, posX]
+	startPos = [ROWS - 1, COLS // 2]
 	if choice == '1':
 		coordinates = display.chooseCoordinates()
 		display.startEasyBot(ROWS, COLS, CELL_SIZE, startPos, coordinates)
@@ -21,8 +23,14 @@ while True:
 	elif choice == '3':
 		print("Hard Bot")
 		print("UNDER CONSTRUCTION")
+	elif choice == '4':
+		print("show Test Cases")
+		coordinates = display.chooseCoordinates()
+		display.showStartingMap(ROWS, COLS, CELL_SIZE, startPos, coordinates)
 	elif choice == 'c':
 		break
 	else:
 		print("Invalid Choice")
 		continue
+
+
